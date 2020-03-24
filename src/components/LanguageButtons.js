c5a4afe6fcf9d00a95aca9d9394eb776a5c7dbb3
Osa Isacson <asaisacson@gmail.com>
2020-03-24 19:42:28 +0100

    hide non-responsive elements on smaller screens

--- src/components/LanguageButtons.js
+++ src/components/LanguageButtons.js
@@ -3,7 +3,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 export default function LanguageButtons() {
   return (
-    <div className="centered">
+    <div className="centered language-buttons">
       <ButtonGroup toggle>
         <Button
           size="sm"
