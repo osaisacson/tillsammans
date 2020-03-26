57fe77d6692471fa9ab14b0ae1f2c12f02c417e4
Osa Isacson <asaisacson@gmail.com>
2020-03-26 23:52:47 +0100

    header styles

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -3,20 +3,27 @@ import { Link } from 'react-router-dom';
 
 export default function MainHeader(props) {
   return (
-    <div className="main-header">
-      <div>
-        <div className="alla-tillsammans">
-          <h3 className="bold">Alla Tillsammans</h3>
-          <h3>mot Covid-19</h3>
+    <>
+      <div className="main-header">
+        <div>
+          <Link to="/">
+            <div className="alla-tillsammans">
+              <h3 className="bold">Alla Tillsammans</h3>
+              <h3>mot Covid-19</h3>
+            </div>
+            <h2>Civilsamhället i samverkan</h2>
+          </Link>
         </div>
-        <h2>Civilsamhället i samverkan</h2>
+        <div className="header-links">
+          <Link to="/sahar-funkar-det"> Såhär funkar det</Link>
+          <Link to="/intro">Vad kan man få hjälp med?</Link>
+          <Link to="/natverk"> Kontakt</Link>
+          <Link to="/vilkor"> Byt språk </Link>
+        </div>
       </div>
-      <div>
-        <Link to="/sahar-funkar-det"> Såhär funkar det</Link>
-        <Link to="/intro">Vad kan man få hjälp med?</Link>
-        <Link to="/natverk"> Kontakt</Link>
-        <Link to="/vilkor"> Byt språk </Link>
+      <div className="header-label">
+        <h3>TJÖRN</h3>
       </div>
-    </div>
+    </>
   );
 }
