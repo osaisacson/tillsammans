e2aa0d0bf140bdc67bd3687687ece93979735e26
Osa Isacson <asaisacson@gmail.com>
2020-08-24 17:51:07 +0200

    update footer copy!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,8 @@ import { Link } from "react-router-dom";
 const Footer = (props) => (
   <div className="footer">
     <p className="bold">Tips!</p>
-    <p>
-      Studiecirklar för Tjörnbor online: 'Våra dialekter' (start vecka 34),
-      'Seniorsurfarna' (start vecka 34)
-    </p>
-    <p>Ny Coronasäkra aktiviteter: 'Prova på Boule' och 'Bygg för barnen!'</p>
+    <p>Studiecirklar för Tjörnbor online: 'Våra dialekter', 'Seniorsurfarna'</p>
+    <p>Coronasäkra aktiviteter: 'Prova på Boule' och 'Bygg för barnen!'</p>
     <p>
       <Link id="tips" to="/tips">
         LÄS MER
