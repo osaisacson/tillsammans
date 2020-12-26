f3feee4aecfa32840173e6a39b4336415d186009
Osa Isacson <asaisacson@gmail.com>
2020-12-26 13:55:29 +0100

    make message clear

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
   <div className="footer">
     <p>Alla Tillsammans är ett ideelt initiativ som stöttar de som behöver temporär hjälp under Coronaepidemins utbrott 2020. 
    <br></br> Vi hjälper främst de som under krisen har hamnat mellan stolarna och behövt en hjälpande hand för att kunna bibehålla sin självkarantän. </p>
-    <p>I denna roll har vi via våra fabulösa volontärer under 2020 utfört nästan 300 ärenden, men från och med årsskiftet kommer vi börja dra ner på vår hjälpverksamhet eftersom vi fyllt vår funktion. De av er som använder plattformen regelbundet kommer bli kontaktade av oss för att tillsammans lösa övergången till långsiktiga lösningar.
+    <p>I denna roll har vi via våra fabulösa volontärer under 2020 utfört nästan 300 ärenden, men <span className="bold">från och med årsskiftet kommer vi börja dra ner på vår hjälpverksamhet </span>eftersom vi fyllt vår funktion. De av er som använder plattformen regelbundet kommer bli kontaktade av oss för att tillsammans lösa övergången till långsiktiga lösningar.
     </p>
 <p>Vi är fortfarande igång, men vill redan nu passa på att varmt tacka alla volontärer och beställare för ert samarbete 2020. Tillsammans har ni dragit ert strå till stacken med att begränsa smittspridningen! För framtiden är vårt samarbete etablerat och vår stödstruktur kommer stå redo att aktiveras närhelst vi behövs. Under tiden - ta hand om er <span role="img" aria-label="heart">❤️</span></p>
     {/* <p>
