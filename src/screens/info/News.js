c9eef9c21889d6245835edddc2c94765cd40a66e
Osa Isacson <asaisacson@gmail.com>
2021-01-05 18:56:59 +0100

    add poems

--- src/screens/info/News.js
+++ src/screens/info/News.js
@@ -3,74 +3,28 @@ import React from "react";
 export default function Partners() {
   return (
     <div className="page-layout">
-      <h1>Sociala Aktiviteter</h1>
+      <h1>SPF Seniorerna Tjörnveteranerna önskar Gott Nytt År!</h1>
       <br />
       <br />
-
-      <h2>Studiecirklar</h2>
 
       <p>
-        I samarbete mellan Alla Tillsammans,{" "}
-        <a href="https://www.spfseniorerna.se/distrikt/bohusdistriktet/foreningslista/Tjornveteranerna/">
-          SPF Tjörnseniorerna
+
+        I år skickar vi en nyårspresent till hyresgäster på Tjörns äldreboenden samt till kunder/patienter som nyttjar kommunens omsorgstjänster inom hemtjänst/hemsjukvård. 
+        Den innehåller en nyinspelad film med Bengtåke Carlson, Hoga som läser kända dikter. 
+
+        </p>
+        <p>
+
+Ta gärna en nyårsfika och lyssna på dikterna i den här länken:
+
+
+      </p>
+<p>
+      <a href="https://www.youtube.com/watch?v=Uh4HgtZdh0g">
+SPF SENIORERNA TJÖRNVETERANERNA DIKTER
         </a>{" "}
-        och Studieförbundet Vuxenskolan är två online studiecirklar nu öppna för
-        intresseanmälan:{" "}
-      </p>
-      <ul>
-        <li>
-          <h6>Våra dialekter</h6> En resa genom historiska dialekter, särskilt
-          Bohusländska, och uttryck till vår egen Tjörndialekt Tjörnbu. Vi lär
-          oss också hur språket utvecklats från vikingatid till nutid genom att
-          titta på några runtexter och medeltida texter. I cirkeln varvas
-          föredrag, klipp och samtal under ledning av Lars Rafstedt. 5
-          tillfällen med 2 h per tillfälle.{" "}
-        </li>
-        <br />
-
-        <li>
-          <h6>Seniorsurfarna</h6> Allt mer i vår vardag styrs av teknik. Om man
-          inte hänger med i utvecklingen riskerar man att snabbt hamna på
-          efterkälken och i värsta fall i utanförskap. I SVT's UR-program
-          'Seniorsurfarna' hjälper Claes Malmberg, Marianne Mörck, Björn
-          Hellberg, Ewa Fröling samt programledaren Kattis Ahlström seniorer att
-          gå från mer eller mindre digitala oskulder till fullfjädrade
-          seniorsurfare. I vår kurs utgår vi från SVT:s studiematerial till
-          serien och lär oss att använda digitala verktyg för att hålla kontakt
-          med vänner och anhöriga och utföra ärenden. 6 tillfällen med 2 h per
-          tillfälle.
-        </li>
-      </ul>
-
-      <p>
-        Intresseanmälan till cirklarna sker direkt till SPF Seniorerna
-        Tjörnveteranerna, telefon 076-8196406 eller mail:
-        magnusson_jan@telia.com (Jan Magnusson).
-      </p>
-
-      <br />
-      <br />
-      <h2>Andra Coronasäkra aktiviteter</h2>
-
-      <ul>
-        <li>
-          PROVA PÅ BOULE: Under sommaren erbjuder SPF Seniorerna
-          Tjörnveteranerna öppen boule på banan vid Kroksdalshallen i Skärhamn.
-          Vi spelar hela sommaren så länge vädret tillåter. Måndagar och
-          torsdagar kl.13.00. Kom när du har tid och lust. Vid frågor kontakta
-          Florence Karlsson, tel. 076-813 02 53.
-        </li>
-        <li>
-          BYGG FÖR TJÖRNS BARN: I ett samarbete mellan SPF Seniorerna
-          Tjörnveteranerna och Egnahemsfabriken sökes pensionärer som vill bygga
-          lekhus till Tjörns barn. Bygg själv hemma eller tillsammans på
-          Egnahemsfabriken, byggmaterial är betalt.{" "}
-          <a href="http://egnahemsfabriken.se/allatillsammans/material/inbjudan-bygg-for-barnen.pdf">
-            Läs mer på denna länk!
-          </a>
-        </li>
-      </ul>
-
+        </p>
+    
       <br />
       <br />
     </div>
