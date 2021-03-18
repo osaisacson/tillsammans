8f5f226ed5ccbe0881bbe8ab8c602554f1eb591a
Osa Isacson <asaisacson@gmail.com>
2021-03-18 10:08:27 +0100

    add new background image, change copy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,16 @@ import React from "react";
 
 const Footer = (props) => (
   <div className="footer">
-    <p>Alla Tillsammans är ett ideelt initiativ som stöttar de som behöver temporär hjälp under Coronaepidemin. 
-   <br></br> Vi hjälper främst de som under krisen har hamnat mellan stolarna och behövt en hjälpande hand för att kunna bibehålla sin självkarantän. </p>
-    <p>I denna roll har vi via våra fabulösa volontärer under 2020 utfört nästan 300 ärenden, men från och med januari 2021 drog vi ner på vår hjälpverksamhet eftersom vi fyllt vår funktion. De som använder plattformen regelbundet kommer hjälpas med att lösa övergången till långsiktiga lösningar.
+    <p>
+      Alla Tillsammans är ett ideellt initiativ som stöttar de som behöver
+      temporär hjälp under Coronaepidemin.
+      <br></br> Vi hjälper främst de som under krisen har hamnat mellan stolarna
+      och behöver en hjälpande hand med ärenden för att kunna bibehålla sin
+      självkarantän.
     </p>
-<p>Vi är fortfarande igång, men vill redan nu passa på att varmt tacka alla volontärer och beställare för ert samarbete. Tillsammans har ni dragit ert strå till stacken med att begränsa smittspridningen 2020! För framtiden är vårt samarbete etablerat och vår stödstruktur kommer stå redo att aktiveras närhelst vi behövs. Under tiden - ta hand om er <span role="img" aria-label="heart">❤️</span></p>
+    {/* <p>I denna roll har vi via våra fabulösa volontärer under 2020 utfört nästan 300 ärenden, men från och med januari 2021 drog vi ner på vår hjälpverksamhet eftersom vi fyllt vår funktion. De som använder plattformen regelbundet kommer hjälpas med att lösa övergången till långsiktiga lösningar.
+    </p>
+<p>Vi är fortfarande igång, men vill redan nu passa på att varmt tacka alla volontärer och beställare för ert samarbete. Tillsammans har ni dragit ert strå till stacken med att begränsa smittspridningen 2020! För framtiden är vårt samarbete etablerat och vår stödstruktur kommer stå redo att aktiveras närhelst vi behövs. Under tiden - ta hand om er <span role="img" aria-label="heart">❤️</span></p> */}
     {/* <p>
       <Link id="tips" to="/tips">
         LÄS MER
